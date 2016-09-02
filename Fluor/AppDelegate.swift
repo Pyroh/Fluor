2bf806afedf82313111b9320cb612b98424ead58
Pyroh <pyroh@icloud.com>
2016-09-02 11:09:12 +0200

    Initial Commit

---
+++ Fluor/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Fluor
+//
+//  Created by Pierre TACCHI on 02/09/16.
+//  Copyright © 2016 Pyrolyse. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
