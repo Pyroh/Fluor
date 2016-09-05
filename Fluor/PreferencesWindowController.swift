d7fc799972b3a9bb7d3e5b108a687555032dc9b0
Pyroh <pyroh@icloud.com>
2016-09-05 21:45:58 +0200

    Some files reorganisation

---
+++ Fluor/PreferencesWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  PreferencesWindowController.swift
+//  Fluor
+//
+//  Created by Pierre TACCHI on 05/09/16.
+//  Copyright © 2016 Pyrolyse. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
