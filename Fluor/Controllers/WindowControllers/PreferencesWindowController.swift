64fb8a7e41da46ed5965b6319345c5889cc2a00e
Pierre TACCHI <pyroh@icloud.com>
2018-02-09 07:55:08 +0100

    Search in app lists now work
    Minor structur changes

---
+++ Fluor/Controllers/WindowControllers/PreferencesWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  PreferencesWindowController.swift
+//  Fluor
+//
+//  Created by Pierre TACCHI on 05/02/2018.
+//  Copyright © 2018 Pyrolyse. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+        let toolbar = window?.toolbar
+        toolbar?.insertItem(withItemIdentifier: .flexibleSpace, at: 2)
+    }
+}
--- Fluor/Misc/AppDelegate.swift
+++ Fluor/Misc/AppDelegate.swift
@@ -20,3 +20,4 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 }
 
+
