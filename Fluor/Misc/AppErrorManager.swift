38765fa499b5c95204ffdeaa9bba7147b5043a04
Pyroh <pyroh@icloud.com>
2020-02-05 17:30:03 +0100

    [UPD] Assets

---
+++ Fluor/Misc/AppErrorManager.swift
@@ -0,0 +1,25 @@
+//
+//  AppExterminator.swift
+//  Fluor
+//
+//  Created by Pierre TACCHI on 10/01/2020.
+//  Copyright © 2020 Pyrolyse. All rights reserved.
+//
+
+import Cocoa
+
+enum AppErrorManager {
+    static func terminateApp(withReason reason: @autoclosure () -> String, andMessage msg: String? = nil) -> Never {
+        let info = reason()
+        let message = msg ?? "Sorry, an unexpected error occured."
+        let alert = NSAlert()
+        
+        alert.alertStyle = .critical
+        alert.informativeText = info
+        alert.messageText = message
+        alert.runModal()
+        
+        NSApp.terminate(self)
+        fatalError(info)
+    }
+}
